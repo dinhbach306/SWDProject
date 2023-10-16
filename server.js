259302efc44de6cb5cddf849c396d6f7a61344b9
magnus <dinhbach3062001@gmail.com>
2023-10-16 23:53:24 +0700

    fix: error connect mongoDB

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE_LOCAL;
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log('DB connection successful!'));
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
