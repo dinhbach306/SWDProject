9b832a55499eecba0aaeb6e54d977afcb2b5c225
magnus <dinhbach3062001@gmail.com>
2023-10-21 20:59:39 +0700

    feat: "crud basic for component"

--- controllers/componentController.js
+++ controllers/componentController.js
@@ -1,6 +1,7 @@
 const Component = require('./../models/entity/component');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/mongoUtils');
 
 exports.createComponent = catchAsync(async (req, res, next) => {
   const newComponent = await Component.create(req.body);
@@ -12,3 +13,60 @@ exports.createComponent = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.getAllComponents = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Component.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const components = await features.query;
+
+  res.status(200).json({
+    status: 'success',
+    results: components.length,
+    data: {
+      components,
+    },
+  });
+});
+
+exports.getComponent = catchAsync(async (req, res, next) => {
+  const component = Component.findById(req.params.id);
+  checkExistComponent(component);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      component,
+    },
+  });
+});
+
+exports.updateComponent = catchAsync(async (req, res, next) => {
+  const component = Component.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, //Nếu không có tạo mới
+    runValidators: true, //Luôn chạy validator
+  });
+  checkExistComponent(component);
+  res.status(204).json({
+    status: 'success',
+    data: {
+      component,
+    },
+  });
+});
+
+exports.deleteComponent = catchAsync(async (req, res, next) => {
+  const component = Component.findByIdAndDelete(req.params.id);
+  checkExistComponent(component);
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+function checkExistComponent(component) {
+  if (!component) {
+    return next(new AppError('No component found with that ID', 404));
+  }
+}
--- routes/componentRouter.js
+++ routes/componentRouter.js
@@ -4,19 +4,16 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router
-  .route('/')
-  // .get(userController.getAllComponents)
-  .post(
-    // authController.protect, // protect all routes after this middleware
-    // authController.restrictTo('admin', 'user'), //authorize
-    componentController.createComponent,
-  );
+router.route('/').get(componentController.getAllComponents).post(
+  authController.protect, // protect all routes after this middleware
+  authController.restrictTo('admin', 'user'), //authorize
+  componentController.createComponent,
+);
 
-// router
-//   .route('/:id')
-//   .get(componentController.getComponent)
-//   .patch(componentController.updateComponent)
-//   .delete(componentController.deleteComponent);
+router
+  .route('/:id')
+  .get(componentController.getComponent)
+  .patch(componentController.updateComponent)
+  .delete(componentController.deleteComponent);
 
 module.exports = router;
