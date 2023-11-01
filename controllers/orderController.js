e92f0eaf7abfed5594bcce9f5aa89dffc399a295
magnus <dinhbach3062001@gmail.com>
2023-11-02 00:25:57 +0700

    get all order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,7 +6,6 @@ const AppError = require('./../utils/appError');
 const APIFeatures = require('./../utils/mongoUtils');
 
 exports.createOrder = catchAsync(async (req, res, next) => {
-  console.log(req.body.cageId, req.body.cageArray)
   //update quantity cage
   const cages = await Cage.find({
     _id: {
@@ -20,7 +19,7 @@ exports.createOrder = catchAsync(async (req, res, next) => {
     const _cageRequest = cageArray.find((item) => item.cageId === cage.id);
     return cage.inStock - _cageRequest.quantity > 0;
   });
-  console.log(isHasCage)
+  console.log(isHasCage);
   if (!isHasCage) {
     return next(new AppError('Not enough cage in stock', 400));
   }
@@ -74,13 +73,19 @@ exports.getAllOrders = catchAsync(async (req, res, next) => {
     .limitFields()
     .paginate();
   const orders = await features.query;
+  const orderByStatus = orders.reduce((result, item) => {
+    if (result[item.status]) {
+      result[item.status].push(item);
+    } else {
+      result[item.status] = [item];
+    }
+    return result;
+  }, {});
 
   res.status(200).json({
     status: 'success',
     results: orders.length,
-    data: {
-      orders,
-    },
+    orderByStatus,
   });
 });
 
